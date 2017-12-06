7d9550d1c5d26dcc72000c0a4a37bd5365766170
David Ball <david@ongoingworlds.com>
2017-12-06 13:50:14 +0000

    New "spinning bot" test application
    For experimenting with embedding WebGL content into a webpage

---
+++ Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float spinX = 0;
+	public float spinY = 0;
+	public float spinZ = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(
+			spinX * Time.deltaTime,
+			spinY * Time.deltaTime,
+			spinZ * Time.deltaTime,
+			Space.Self
+		);
+	}
+}
